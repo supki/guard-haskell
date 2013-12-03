860eae4d0c6b21cb5ccc8b2562f00ef106eae52c
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-12-03 18:00:11 +0400

    Refactor the communication with repl instance

--- lib/guard/haskell/repl.rb
+++ lib/guard/haskell/repl.rb
@@ -1,11 +1,25 @@
 require 'open3'
 
 class ::Guard::Haskell::Repl
-  attr_reader :stdin, :reader, :thread, :result
+  attr_reader :stdin, :listener, :thread, :result
 
   def initialize
     @running = false
     @result  = :success
+  end
+
+  def self.test(str)
+    case str
+    when /\d+ examples?, 0 failures/
+      :success
+    when /\d+ examples?, \d+ failures?/
+      :runtime_failure
+    when /Failed, modules loaded:/,
+         /\*{3} Exception:/,
+         /phase `C pre-processor' failed/,
+         /GHCi runtime linker: fatal error:/
+      :compile_failure
+    end
   end
 
   def start ghci_options
@@ -16,50 +30,28 @@ class ::Guard::Haskell::Repl
     cmd.concat(ghci_options)
 
     @stdin, stdout, @thread = ::Open3.popen2e(*cmd)
-    @reader = ::Thread.new do
-      loop do
-        while (out = stdout.gets)
-          print out
-          if @running
-            case out
-            when /\d+ examples?, 0 failures/
-              @result  = :success
-              @running = false
-            when /\d+ examples?, \d+ failures?/
-              @result  = :runtime_failure
-              @running = false
-            when /Failed, modules loaded:/,
-                 /\*{3} Exception:/,
-                 /phase `C preprocessor' failed/,
-                 /^GHCi runtime linker: fatal error:/
-              @result  = :compile_failure
-              @running = false
-            end
-          end
-        end
-      end
-    end
+    @listener = ::Thread.new { listen(stdout, STDOUT) }
   end
 
-  def init spec
-    _repl ":load #{spec}\n"
+  def init(spec)
+    repl(":load #{spec}\n")
   end
 
   def exit
-    ::Process::kill "TERM", thread.pid
-    ::Thread.kill(reader)
+    ::Process::kill("TERM", thread.pid)
+    ::Thread.kill(listener)
   end
 
-  def run pattern = nil
+  def run(pattern = nil)
     if pattern.nil?
-      _repl ":reload\n:main --color\n"
+      repl(":reload\n:main --color\n")
     else
-      _repl ":reload\n:main --color --match #{pattern}\n"
+      repl(":reload\n:main --color --match #{pattern}\n")
     end
   end
 
   def rerun
-    _repl ":reload\n:main --color --rerun\n"
+    repl(":reload\n:main --color --rerun\n")
   end
 
   def result
@@ -67,8 +59,24 @@ class ::Guard::Haskell::Repl
     @result
   end
 
-  def _repl command
-    @running = true
-    stdin.write command
-  end
+  private
+
+    def repl(command)
+      @running = true
+      stdin.write(command)
+    end
+
+    def listen(in_stream, out_stream)
+      while (str = in_stream.gets)
+        out_stream.print(str)
+        if @running
+          res = self.class.test(str)
+          case res
+          when :success, :runtime_failure, :compile_failure
+            @result  = res
+            @running = false
+          end
+        end
+      end
+    end
 end
--- spec/guard/haskell/repl_spec.rb
+++ spec/guard/haskell/repl_spec.rb
@@ -8,42 +8,188 @@ describe ::Guard::Haskell::Repl do
 
   describe '#init' do
     it "calls :load on ghci instance" do
-      expect(repl).to receive(:_repl).with(/:load/)
+      expect(repl).to receive(:repl).with(/:load/)
       repl.init("Spec.hs")
     end
 
     it "loads provided spec" do
-      expect(repl).to receive(:_repl).with(/FooBarSpec.hs/)
+      expect(repl).to receive(:repl).with(/FooBarSpec.hs/)
       repl.init("FooBarSpec.hs")
     end
   end
 
   describe '#run' do
     it "reloads the spec if no pattern is provided" do
-      expect(repl).to receive(:_repl).with(/:reload/)
+      expect(repl).to receive(:repl).with(/:reload/)
       repl.run
     end
 
     it "reloads the spec if a pattern is provided" do
-      expect(repl).to receive(:_repl).with(/:reload/)
+      expect(repl).to receive(:repl).with(/:reload/)
       repl.run("FooBar")
     end
 
     it "provides a pattern for spec to match" do
-      expect(repl).to receive(:_repl).with(/--match FooBar/)
+      expect(repl).to receive(:repl).with(/--match FooBar/)
       repl.run("FooBar")
     end
   end
 
   describe '#rerun' do
     it "reloads the spec" do
-      expect(repl).to receive(:_repl).with(/:reload/)
+      expect(repl).to receive(:repl).with(/:reload/)
       repl.rerun
     end
 
     it "reruns the spec" do
-      expect(repl).to receive(:_repl).with(/--rerun/)
+      expect(repl).to receive(:repl).with(/--rerun/)
       repl.rerun
+    end
+  end
+
+  describe '#test' do
+    it "handles zero examples/failures" do
+      expect(::Guard::Haskell::Repl.test("0 examples, 0 failures")).to eq(:success)
+    end
+
+    it "handles one example/zero failures" do
+      expect(::Guard::Haskell::Repl.test("1 example, 0 failures")).to eq(:success)
+    end
+
+    it "handles multiple examples/zero failures" do
+      expect(::Guard::Haskell::Repl.test("37 examples, 0 failures")).to eq(:success)
+    end
+
+    it "handles one example/failure" do
+      expect(::Guard::Haskell::Repl.test("1 example, 1 failure")).to eq(:runtime_failure)
+    end
+
+    it "handles multiple examples/multiple failures" do
+      expect(::Guard::Haskell::Repl.test("26 examples, 2 failures")).to eq(:runtime_failure)
+    end
+
+    it "handles failure to load the module" do
+      expect(::Guard::Haskell::Repl.test("Failed, modules loaded:")).to eq(:compile_failure)
+    end
+
+    it "handles uncaught exceptions" do
+      expect(::Guard::Haskell::Repl.test("*** Exception: Prelude.undefined")).to eq(:compile_failure)
+    end
+
+    it "handles CPP errors" do
+      expect(::Guard::Haskell::Repl.test("phase `C pre-processor' failed")).to eq(:compile_failure)
+    end
+
+    it "handles runtime linker errors" do
+      expect(::Guard::Haskell::Repl.test("GHCi runtime linker: fatal error:")).to eq(:compile_failure)
+    end
+  end
+
+  describe '#listen' do
+    context 'real world' do
+      it "handles typical passed run" do
+        in_stream = ::StringIO.open(<<-FOO)
+          Useful.Git
+            fromGraph
+              - creates `git init' script from empty graph
+              - creates git script from two-node graph
+              - creates git script from three-node graph
+              - creates git script from three-node chain graph
+
+          Finished in 0.0054 seconds
+          4 examples, 0 failures
+        FOO
+        out_stream = File.open("/dev/null", "w")
+        repl.instance_variable_set(:@running, true)
+
+        repl.send(:listen, in_stream, out_stream)
+
+        expect(repl.instance_variable_get(:@running)).to be_false
+        expect(repl.instance_variable_get(:@result)).to eq(:success)
+      end
+
+      it "handles typical failed spec run" do
+        in_stream = ::StringIO.open(<<-FOO)
+          Useful.Git
+            fromGraph
+              - creates `git init' script from empty graph
+              - creates git script from two-node graph FAILED [1]
+              - creates git script from three-node graph
+              - creates git script from three-node chain graph
+
+          1) Useful.Git.fromGraph creates git script from two-node graph
+          expected: Just [InitE,OrphanE "foo" "7",CommitE "bar" ["1"] "2"]
+           but got: Just [InitE,OrphanE "foo" "1",CommitE "bar" ["1"] "2"]
+
+          Randomized with seed 4611685481380198536
+
+          Finished in 0.0089 seconds
+          4 examples, 1 failure
+          *** Exception: ExitFailure 1
+        FOO
+        out_stream = File.open("/dev/null", "w")
+        repl.instance_variable_set(:@running, true)
+
+        repl.send(:listen, in_stream, out_stream)
+
+        expect(repl.instance_variable_get(:@running)).to be_false
+        expect(repl.instance_variable_get(:@result)).to eq(:runtime_failure)
+      end
+
+      it "handles runtime linker error" do
+        in_stream = ::StringIO.open(<<-FOO)
+          GHCi runtime linker: fatal error: I found a duplicate definition for symbol
+             HUnitzm1zi2zi5zi2_TestziHUnitziBase_zdwzdcshowsPrec_slow
+          whilst processing object file
+             /home/maksenov/.cabal/lib/HUnit-1.2.5.2/ghc-7.6.2/HSHUnit-1.2.5.2.o
+          This could be caused by:
+             * Loading two different object files which export the same symbol
+             * Specifying the same object file twice on the GHCi command line
+             * An incorrect `package.conf' entry, causing some object to be
+               loaded twice.
+          GHCi cannot safely continue in this situation.  Exiting now.  Sorry.
+        FOO
+        out_stream = File.open("/dev/null", "w")
+        repl.instance_variable_set(:@running, true)
+
+        repl.send(:listen, in_stream, out_stream)
+
+        expect(repl.instance_variable_get(:@running)).to be_false
+        expect(repl.instance_variable_get(:@result)).to eq(:compile_failure)
+      end
+
+      it "handles hspec exceptions" do
+        in_stream = ::StringIO.open(<<-FOO)
+          *Main> Ok, modules loaded: Main, Useful.Parser, Useful.Graph, Useful.Git.
+          *Main>
+          *** Exception: Prelude.undefined
+        FOO
+        out_stream = File.open("/dev/null", "w")
+        repl.instance_variable_set(:@running, true)
+
+        repl.send(:listen, in_stream, out_stream)
+
+        expect(repl.instance_variable_get(:@running)).to be_false
+        expect(repl.instance_variable_get(:@result)).to eq(:compile_failure)
+      end
+
+      it "handles CPP exceptions" do
+        in_stream = ::StringIO.open(<<-FOO)
+          *Main>
+          test/Useful/GitSpec.hs:4:0:
+               error: invalid preprocessing directive #ifd
+               #ifd
+               ^
+          phase `C pre-processor' failed (exitcode = 1)
+        FOO
+        out_stream = File.open("/dev/null", "w")
+        repl.instance_variable_set(:@running, true)
+
+        repl.send(:listen, in_stream, out_stream)
+
+        expect(repl.instance_variable_get(:@running)).to be_false
+        expect(repl.instance_variable_get(:@result)).to eq(:compile_failure)
+      end
     end
   end
 end
